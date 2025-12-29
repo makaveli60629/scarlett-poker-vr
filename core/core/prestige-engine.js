325c26d4d14d3c7723ab95ea32d093abc4cf143e
makaveli60629 <ronaldchadee@gmail.com>
2025-12-29 15:12:11 -0600

    Add player rank initialization and update function
    Initialize PLAYER_RANK and define RANKS array. Add updateRank function to change rank based on bank value.

--- core/core/prestige-engine.js
+++ core/core/prestige-engine.js
@@ -1,1 +1,8 @@
+window.PLAYER_RANK = "BRONZE";
 
+const RANKS = ["BRONZE","SILVER","GOLD","CRIMSON"];
+
+function updateRank(bank) {
+  if (bank > 100000) window.PLAYER_RANK = "GOLD";
+  if (bank > 250000) window.PLAYER_RANK = "CRIMSON";
+}

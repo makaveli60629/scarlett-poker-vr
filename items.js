dfb878b823357b9d6247730016dfce2b4165009c
makaveli60629 <ronaldchadee@gmail.com>
2025-12-31 01:08:16 -0600

    Update items.js

--- items.js
+++ items.js
@@ -1,13 +1,5 @@
-// items.js - Item 3D Shapes
-
 AFRAME.registerComponent('item-display', {
   init: function () {
-    if (this.el.classList.contains('tshirt')) {
-      this.el.setAttribute('geometry', {primitive: 'box', width: 0.5, height: 0.6, depth: 0.1});
-      this.el.setAttribute('material', {color: this.el.getAttribute('data-color') || 'blue'});
-    }
-    if (this.el.classList.contains('guitar')) {
-      this.el.setAttribute('geometry', {primitive: 'box', width: 0.1, height: 0.8, depth: 0.05});
-    }
+    // Reserved for Update 1.5 T-shirt Textures
   }
 });

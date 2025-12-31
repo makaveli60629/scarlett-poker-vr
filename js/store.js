209d3cc76c29fee253a85592976f405fd8ee2f43
makaveli60629 <ronaldchadee@gmail.com>
2025-12-31 12:03:19 -0600

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,6 +1,6 @@
 window.onload = () => {
-  if (!localStorage.getItem('poker_wallet')) {
+  if(!localStorage.getItem('poker_wallet')) {
     localStorage.setItem('poker_wallet', '1000');
   }
-  console.log("Makaveli 62629 - Scarlett Poker VR 1.3 Online");
+  console.log("Update 1.3 Logic Locked. Ready for 1.4 Textures.");
 };

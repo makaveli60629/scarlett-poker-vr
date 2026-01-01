fe431e04adefa410030010f2125ee81b5215fc58
makaveli60629 <ronaldchadee@gmail.com>
2026-01-01 10:19:44 -0600

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -1,10 +1,10 @@
-// 1.5.1 Mega Particles Optimized
 AFRAME.registerComponent('mega-particles', {
     init: function () {
         this.el.setAttribute('particle-system', {
             preset: 'gold',
             particleCount: 2000,
             size: 0.1,
+            opacity: 0.7,
             color: '#FFD700'
         });
     }

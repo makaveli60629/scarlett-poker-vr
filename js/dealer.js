a323ea9a595d6c420561415277e91d64efaf1ab9
makaveli60629 <ronaldchadee@gmail.com>
2026-01-03 16:14:14 -0600

    Update dealer.js

--- js/dealer.js
+++ js/dealer.js
@@ -1,17 +1,22 @@
-import * as THREE from 'three';
-
 export const Dealer = {
     deck: [],
+
     init(scene) {
         const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-        const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-        for (let s of suits) {
-            for (let v of values) {
-                this.deck.push({ s, v });
+        const values = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+        this.deck.length = 0;
+
+        for (const s of suits) {
+            for (const v of values) {
+                this.deck.push({ suit: s, value: v });
             }
         }
+
         this.shuffle();
+        console.log("Deck ready:", this.deck.length, "cards");
     },
+
     shuffle() {
         for (let i = this.deck.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));

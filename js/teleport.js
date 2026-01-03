e6e4732e1aa8c779e31d942e5e557a4d3286c302
makaveli60629 <ronaldchadee@gmail.com>
2026-01-02 21:24:42 -0600

    Update teleport.js

--- js/teleport.js
+++ js/teleport.js
@@ -1,17 +1,46 @@
-// teleport.js
-AFRAME.registerComponent('teleport-controls', {
-  init() {
-    const el = this.el;
-    el.addEventListener('click', (evt) => {
-      const rig = document.querySelector('#rig');
-      if (!evt.detail.intersection) return;
+import * as THREE from 'three';
 
-      const point = evt.detail.intersection.point;
-      rig.setAttribute('position', {
-        x: point.x,
-        y: 1.6,
-        z: point.z
-      });
-    });
-  }
-});
+export class TeleportSystem {
+    constructor(renderer, camera, scene, world) {
+        this.renderer=renderer;
+        this.camera=camera;
+        this.scene=scene;
+        this.world=world;
+
+        this.raycaster=new THREE.Raycaster();
+        this.laser=null;
+        this.targetPos=new THREE.Vector3();
+
+        this.initLaser();
+        this.setupController();
+    }
+
+    initLaser() {
+        const mat = new THREE.LineBasicMaterial({ color:0x00ff00 });
+        const geo = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0,0,0), new THREE.Vector3(0,0,-1)]);
+        this.laser = new THREE.Line(geo, mat);
+        this.laser.scale.z=10;
+        this.scene.add(this.laser);
+    }
+
+    setupController() {
+        const controller = this.renderer.xr.getController(0); // left hand
+        controller.addEventListener('selectstart',()=>this.teleport());
+        this.scene.add(controller);
+
+        controller.add(this.laser);
+    }
+
+    teleport() {
+        // Cast ray from laser
+        const dir = new THREE.Vector3(0,0,-1).applyQuaternion(this.laser.quaternion);
+        const origin = this.laser.getWorldPosition(new THREE.Vector3());
+        this.raycaster.set(origin, dir);
+        const intersects = this.raycaster.intersectObjects(this.scene.children,true);
+
+        if(intersects.length>0){
+            const point=intersects[0].point;
+            this.camera.position.set(point.x,1.6,point.z);
+        }
+    }
+}

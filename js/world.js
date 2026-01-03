841e797371ab7051526b1ea9bb8c90bd661d906e
makaveli60629 <ronaldchadee@gmail.com>
2026-01-02 22:11:08 -0600

    Update world.js

--- js/world.js
+++ js/world.js
@@ -5,51 +5,43 @@ export class World {
     constructor(scene, renderer) {
         this.scene = scene;
         this.renderer = renderer;
-        this.handFactory = new XRHandModelFactory();
-
         this.setupLights();
         this.setupEnvironment();
         this.setupHands();
     }
 
     setupLights() {
-        // Boosted lighting to prevent black objects
-        const ambient = new THREE.AmbientLight(0xffffff, 1.0);
+        // High intensity ambient light so everything is visible
+        const ambient = new THREE.AmbientLight(0xffffff, 1.5);
         this.scene.add(ambient);
-
-        const sun = new THREE.DirectionalLight(0xffffff, 1.5);
-        sun.position.set(5, 10, 7);
-        this.scene.add(sun);
     }
 
     setupEnvironment() {
-        // Green Poker Table
-        const tableGeo = new THREE.CylinderGeometry(1.2, 1.2, 0.1, 32);
-        const tableMat = new THREE.MeshStandardMaterial({ color: 0x006600 });
-        const table = new THREE.Mesh(tableGeo, tableMat);
-        table.position.set(0, 0.7, -1); // Placed in front of user
-        this.scene.add(table);
+        // A simple white cube at the center to test visibility
+        const boxGeo = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+        const boxMat = new THREE.MeshStandardMaterial({ color: 0xffffff });
+        const box = new THREE.Mesh(boxGeo, boxMat);
+        box.position.set(0, 1.2, -1); // Right in front of your face
+        this.scene.add(box);
 
-        // Floor (Grid)
-        const grid = new THREE.GridHelper(10, 10);
+        // Grid floor
+        const grid = new THREE.GridHelper(10, 10, 0xffffff, 0x444444);
         this.scene.add(grid);
     }
 
     setupHands() {
-        // Controller 0 (Left Hand)
-        this.hand1 = this.renderer.xr.getHand(0);
-        this.hand1.add(this.handFactory.createHandModel(this.hand1, 'mesh'));
-        this.scene.add(this.hand1);
-
-        // Controller 1 (Right Hand)
-        this.hand2 = this.renderer.xr.getHand(1);
-        this.hand2.add(this.handFactory.createHandModel(this.hand2, 'mesh'));
-        this.scene.add(this.hand2);
+        this.handFactory = new XRHandModelFactory();
         
-        console.log("Hands initialized - controllers hidden as per update instructions.");
+        // Left Hand
+        const hand1 = this.renderer.xr.getHand(0);
+        hand1.add(this.handFactory.createHandModel(hand1, 'mesh'));
+        this.scene.add(hand1);
+
+        // Right Hand
+        const hand2 = this.renderer.xr.getHand(1);
+        hand2.add(this.handFactory.createHandModel(hand2, 'mesh'));
+        this.scene.add(hand2);
     }
 
-    update() {
-        // Logic for hand movement or physics goes here
-    }
+    update() {}
 }

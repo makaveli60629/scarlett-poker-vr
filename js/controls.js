e74e664595abd48d4e9b1f1adaaf0bfeed97569b
makaveli60629 <ronaldchadee@gmail.com>
2026-01-03 15:36:03 -0600

    Update controls.js

--- js/controls.js
+++ js/controls.js
@@ -3,57 +3,54 @@ import { XRHandModelFactory } from 'three/addons/webxr/XRHandModelFactory.js';
 
 export const Controls = {
     hands: [],
-    laser: new THREE.Line(),
-    watchMenu: new THREE.Mesh(),
+    laser: null,
+    watchMenu: null,
 
-    init(renderer, scene, playerGroup, camera) {
+    init(renderer, scene, playerGroup) {
         const factory = new XRHandModelFactory();
         for (let i = 0; i < 2; i++) {
             const hand = renderer.xr.getHand(i);
-            const model = factory.createHandModel(hand, 'mesh');
-            hand.add(model);
+            hand.add(factory.createHandModel(hand, 'mesh'));
             playerGroup.add(hand);
-            this.hands.push({ hand, model });
+            this.hands.push(hand);
         }
 
-        // OBJECT 2: THE WRIST WATCH (Attached to left hand)
+        // Wrist Watch Menu (Left Hand)
         this.watchMenu = new THREE.Mesh(
             new THREE.PlaneGeometry(0.1, 0.05),
             new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.DoubleSide })
         );
         this.watchMenu.visible = false;
-        this.hands[0].hand.add(this.watchMenu); // Left Hand
+        this.hands[0].add(this.watchMenu);
         this.watchMenu.position.set(0, 0.05, 0);
         this.watchMenu.rotation.x = -Math.PI / 2;
 
-        // Laser for Teleport
-        const geo = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0,0,0), new THREE.Vector3(0,0,-1)]);
-        this.laser = new THREE.Line(geo, new THREE.LineBasicMaterial({ color: 0x00ff00 }));
+        // Teleport Laser
+        const geo = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0,0,0), new THREE.Vector3(0,0,-5)]);
+        this.laser = new THREE.Line(geo, new THREE.LineBasicMaterial({ color: 0x00ffff }));
         this.laser.visible = false;
         scene.add(this.laser);
     },
 
     update(renderer, camera, playerGroup) {
-        this.hands.forEach((h, i) => {
-            const indexTip = h.hand.get(8);
-            const thumbTip = h.hand.get(4);
-            const wrist = h.hand.get(0);
+        this.hands.forEach((hand, i) => {
+            const indexTip = hand.get(8);
+            const thumbTip = hand.get(4);
+            const wrist = hand.get(0);
 
             if (indexTip && thumbTip) {
-                const dist = indexTip.position.distanceTo(thumbTip.position);
-                if (dist < 0.02) {
+                const isPinching = indexTip.position.distanceTo(thumbTip.position) < 0.02;
+                if (isPinching) {
                     this.laser.visible = true;
                     this.laser.position.copy(indexTip.position);
-                    // Add teleport jump logic here
                 } else {
                     this.laser.visible = false;
                 }
             }
-
-            // Wrist Flip for Watch Menu
-            if (i === 0 && wrist) { // Left hand watch
+            
+            if (i === 0 && wrist) { // Watch Check
                 const normal = new THREE.Vector3(0, 1, 0).applyQuaternion(wrist.quaternion);
-                this.watchMenu.visible = normal.y > 0.5; // Only show if wrist is flipped up
+                this.watchMenu.visible = normal.y > 0.6;
             }
         });
     }

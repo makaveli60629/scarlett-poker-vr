6c43506844e09b66c3f0785b160b2c0ff594a10b
makaveli60629 <ronaldchadee@gmail.com>
2026-01-03 15:32:54 -0600

    Update dealer.js

--- js/dealer.js
+++ js/dealer.js
@@ -2,28 +2,27 @@ import * as THREE from 'three';
 
 export const Dealer = {
     deck: [],
-    initDeck() {
-        const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-        const values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-        for (let s of suits) {
-            for (let v of values) {
+    suits: ['Hearts', 'Diamonds', 'Clubs', 'Spades'],
+    values: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+
+    init(scene) {
+        this.generateDeck();
+        this.shuffle();
+    },
+
+    generateDeck() {
+        this.deck = [];
+        for (let s of this.suits) {
+            for (let v of this.values) {
                 this.deck.push({ suit: s, value: v });
             }
         }
-        this.shuffle();
     },
+
     shuffle() {
         for (let i = this.deck.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
             [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
-        }
-    },
-    dealFlop(scene) {
-        for (let i = 0; i < 3; i++) {
-            const cardData = this.deck.pop();
-            const card = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.005, 0.15), new THREE.MeshStandardMaterial({ color: 0xffffff }));
-            card.position.set(-0.2 + (i * 0.2), 0.86, 0);
-            scene.add(card);
         }
     }
 };

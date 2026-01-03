de157865f74e2eaf959d2a25fa13803444a80de5
makaveli60629 <ronaldchadee@gmail.com>
2026-01-02 21:30:47 -0600

    Update teleport.js

--- js/teleport.js
+++ js/teleport.js
@@ -1,45 +1,41 @@
 import * as THREE from 'three';
 
 export class TeleportSystem {
-    constructor(renderer, camera, scene, world) {
+    constructor(renderer,camera,scene,world){
         this.renderer=renderer;
         this.camera=camera;
         this.scene=scene;
         this.world=world;
-
         this.raycaster=new THREE.Raycaster();
         this.laser=null;
-        this.targetPos=new THREE.Vector3();
-
         this.initLaser();
         this.setupController();
     }
 
-    initLaser() {
-        const mat = new THREE.LineBasicMaterial({ color:0x00ff00 });
+    initLaser(){
+        const mat = new THREE.LineBasicMaterial({color:0x00ff00});
         const geo = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(0,0,0), new THREE.Vector3(0,0,-1)]);
-        this.laser = new THREE.Line(geo, mat);
+        this.laser=new THREE.Line(geo,mat);
         this.laser.scale.z=10;
         this.scene.add(this.laser);
     }
 
-    setupController() {
-        const controller = this.renderer.xr.getController(0); // left hand
+    setupController(){
+        const controller = this.renderer.xr.getController(0);
         controller.addEventListener('selectstart',()=>this.teleport());
         this.scene.add(controller);
-
         controller.add(this.laser);
     }
 
-    teleport() {
-        // Cast ray from laser
-        const dir = new THREE.Vector3(0,0,-1).applyQuaternion(this.laser.quaternion);
-        const origin = this.laser.getWorldPosition(new THREE.Vector3());
-        this.raycaster.set(origin, dir);
-        const intersects = this.raycaster.intersectObjects(this.scene.children,true);
+    teleport(){
+        const dir=new THREE.Vector3(0,0,-1).applyQuaternion(this.laser.quaternion);
+        const origin=this.laser.getWorldPosition(new THREE.Vector3());
+        this.raycaster.set(origin,dir);
+        const intersects=this.raycaster.intersectObjects(this.scene.children,true);
 
         if(intersects.length>0){
             const point=intersects[0].point;
+            // Safe Y position
             this.camera.position.set(point.x,1.6,point.z);
         }
     }

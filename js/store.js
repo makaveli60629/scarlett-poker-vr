b431e017ef6067781733c6e00b918c8e6c121319
makaveli60629 <ronaldchadee@gmail.com>
2026-01-03 21:56:42 -0600

    Create store.js

---
+++ js/store.js
@@ -0,0 +1,9 @@
+export const Store = {
+  items: [
+    { id: "chips_1000", name: "1,000 Chips", price: 0 },
+    { id: "chips_5000", name: "5,000 Chips", price: 0 },
+    { id: "chips_10000", name: "10,000 Chips", price: 0 },
+    { id: "skin_gold", name: "Gold Trim Skin", price: 25000 },
+  ],
+  open: false
+};

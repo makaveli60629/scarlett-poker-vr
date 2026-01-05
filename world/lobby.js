ee5092a08d14947e15f0d0f0cad0a391cfab3c95
makaveli60629 <ronaldchadee@gmail.com>
2026-01-04 22:27:57 -0600

    Delete world directory

--- world/lobby.js
+++
@@ -1,7 +0,0 @@
-// Inside buildPermanentLobby
-const storePortal = new THREE.Group();
-// ... (your ring and glow code)
-storePortal.userData = { dest: 'STORE' }; // THIS IS THE KEY
-storePortal.position.set(5.5, 1.5, 0); 
-scene.add(storePortal);
-portals.push(storePortal);

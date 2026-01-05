da765cefcd62d36b89986494798cea459ab83bd0
makaveli60629 <ronaldchadee@gmail.com>
2026-01-05 14:12:10 -0600

    Create three.js

---
+++ js/three.js
@@ -0,0 +1,5 @@
+// js/three.js
+// Shim so GitHub Pages can resolve THREE without importmaps.
+// Every file should import THREE like: import * as THREE from "./three.js";
+
+export * from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";

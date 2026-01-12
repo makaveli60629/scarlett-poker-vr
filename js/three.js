b4d517ee016772f71d8fd3ed45dd2a47a0a41c68
makaveli60629 <ronaldchadee@gmail.com>
2026-01-11 18:09:27 -0600

    Update three.js

--- js/three.js
+++ js/three.js
@@ -1,6 +1,5 @@
 // /js/three.js — Scarlett Three Wrapper (NO "three" bare imports anywhere)
 
 export * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
-
 export { VRButton } from "https://unpkg.com/three@0.160.0/examples/jsm/webxr/VRButton.js";
 export { XRControllerModelFactory } from "https://unpkg.com/three@0.160.0/examples/jsm/webxr/XRControllerModelFactory.js";

9379a303e4f360157549e36b04bebb27173155e5
makaveli60629 <ronaldchadee@gmail.com>
2026-01-14 15:50:18 -0600

    Update hud.js

--- js/hud.js
+++ js/hud.js
@@ -1,7 +1,6 @@
 export const HUD = {
-    init(ctx) {
-        const h = document.getElementById('hud');
-        // Double tap or double click to hide
-        window.addEventListener('dblclick', () => h.classList.toggle('hidden'));
-    }
-};
+  init(ctx) {
+    const h = document.getElementById('hud')
+    h.addEventListener('dblclick', () => h.classList.toggle('hidden'))
+  }
+}

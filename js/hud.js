37a0c4ef90f49e9deff1b465553826444c737608
makaveli60629 <ronaldchadee@gmail.com>
2026-01-14 15:21:26 -0600

    Update hud.js

--- js/hud.js
+++ js/hud.js
@@ -1,11 +1,7 @@
 export const HUD = {
     init(ctx) {
         const h = document.getElementById('hud');
-        
-        // This listener allows you to toggle the diagnostic view
-        window.addEventListener('dblclick', () => {
-            h.classList.toggle('hidden');
-            console.log('[HUD] Toggle visibility');
-        });
+        // Double tap or double click to hide
+        window.addEventListener('dblclick', () => h.classList.toggle('hidden'));
     }
 };

7bdf7014ceb495b6193f6e8c4794c39bdc4991e8
makaveli60629 <ronaldchadee@gmail.com>
2026-01-14 15:49:07 -0600

    Update input.js

--- js/input.js
+++ js/input.js
@@ -1,16 +1,17 @@
 export const Input = {
-    init(ctx) {
-        const { renderer, scene } = ctx.world;
-        
-        // Setup Hand Tracking
-        this.hand1 = renderer.xr.getHand(0);
-        this.hand2 = renderer.xr.getHand(1);
-        scene.add(this.hand1, this.hand2);
-        
-        // Add visual cues for hands (simple cubes for now)
-        const geo = new THREE.BoxGeometry(0.05, 0.05, 0.05);
-        const mat = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
-        this.hand1.add(new THREE.Mesh(geo, mat));
-        this.hand2.add(new THREE.Mesh(geo, mat));
+  init(ctx) {
+    this.controllers = []
+
+    if (ctx.world.renderer.xr.enabled) {
+      for (let i=0;i<2;i++) {
+        const c = ctx.world.renderer.xr.getController(i)
+        c.addEventListener('connected', () => ctx.Diagnostics.ok('Controller '+i))
+        ctx.world.scene.add(c)
+        this.controllers.push(c)
+      }
     }
-};
+
+    window.addEventListener('touchstart', () => ctx.Diagnostics.ok('Touch Input'))
+    window.addEventListener('mousedown', () => ctx.Diagnostics.ok('Mouse Input'))
+  }
+}

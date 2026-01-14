d169b23b617489bd4ff56355ad43029ee73592dd
makaveli60629 <ronaldchadee@gmail.com>
2026-01-14 15:20:59 -0600

    Update table.js

--- js/table.js
+++ js/table.js
@@ -1,13 +1,9 @@
 export const Tables = {
     build(scene) {
-        const tableGeo = new THREE.CylinderGeometry(1.2, 1.2, 0.1, 32);
-        const tableMat = new THREE.MeshStandardMaterial({ color: 0x004400 });
-        const table = new THREE.Mesh(tableGeo, tableMat);
-        
-        // Height of 0.8 meters is standard table height
-        table.position.y = 0.8; 
-        
-        scene.add(table);
-        console.log('[TABLE] Poker table generated.');
+        const geo = new THREE.CylinderGeometry(1.2, 1.2, 0.1, 32);
+        const mat = new THREE.MeshStandardMaterial({ color: 0x004400 });
+        const mesh = new THREE.Mesh(geo, mat);
+        mesh.position.y = 0.8;
+        scene.add(mesh);
     }
 };

a4e8d2f5894d8960280777f1439bbe3bf38d4578
makaveli60629 <ronaldchadee@gmail.com>
2026-01-14 15:20:05 -0600

    Update spawn_points.js

--- js/spawn_points.js
+++ js/spawn_points.js
@@ -1,7 +1,5 @@
 export const SpawnPoints = {
     apply(camera) {
-        // Sets player at human height (1.6) and slightly back (2)
-        camera.position.set(0, 1.6, 2); 
-        console.log('[SPAWN] Player positioned at table.');
+        camera.position.set(0, 1.6, 2.5);
     }
 };

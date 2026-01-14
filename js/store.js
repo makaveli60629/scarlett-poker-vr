a9eecebbf5556c85640fad4dbd53e36ae5e0ed94
makaveli60629 <ronaldchadee@gmail.com>
2026-01-14 15:19:06 -0600

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,20 +1,6 @@
 export const Store = {
     init(ctx) {
         this.ctx = ctx;
-        this.catalog = [
-            { id: 'gold_skin', name: 'Gold Hand Texture', price: 500, texture: 'assets/textures/gold_hand.jpg' },
-            { id: 'royal_table', name: 'Royal Felt', price: 1000, texture: 'assets/textures/table_royal.jpg' }
-        ];
-        this.inventory = [];
-        ctx.Diagnostics.ok('Store Module Loaded');
-    },
-
-    purchase(itemId) {
-        const item = this.catalog.find(i => i.id === itemId);
-        if (item) {
-            this.inventory.push(item);
-            this.ctx.Diagnostics.log(`Purchased: ${item.name}`);
-            // Here we would trigger the texture swap logic
-        }
+        // Placeholder for texture loading from /assets/textures/
     }
 };

20d1c9568d7afc81a4715eefc00b2dad599db876
makaveli60629 <ronaldchadee@gmail.com>
2026-01-14 15:19:40 -0600

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,15 +1,6 @@
 export const Admin = {
     init(ctx) {
         this.ctx = ctx;
-        window.scarlettAdmin = {
-            resetWorld: () => location.reload(),
-            giveMoney: (amt) => ctx.Diagnostics.log(`Admin: Added ${amt} credits`),
-            debugMode: () => {
-                ctx.world.scene.traverse(obj => {
-                    if (obj.isMesh) obj.material.wireframe = !obj.material.wireframe;
-                });
-            }
-        };
-        ctx.Diagnostics.ok('Admin Controls Active (window.scarlettAdmin)');
+        window.adminReset = () => location.reload();
     }
 };

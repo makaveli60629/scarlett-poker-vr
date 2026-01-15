958ab6da9c4d08c9740a5a78e74204219a849a57
makaveli60629 <ronaldchadee@gmail.com>
2026-01-15 16:35:42 -0600

    Update world_materials.js

--- js/scarlett1/world_materials.js
+++ js/scarlett1/world_materials.js
@@ -1,56 +1,50 @@
-import * as THREE from "https://unpkg.com/three@0.158.0/build/three.module.js";
-import { makeGridCarpetTexture } from "./world_helpers.js";
+// /js/scarlett1/world_materials.js
+export function makeMaterials(THREE, { quality = "quest" } = {}) {
+  const low = (quality === "quest");
 
-export function createMaterials(quality = "quest") {
-  const carpetTex = makeGridCarpetTexture(512);
-
-  const matCarpet = new THREE.MeshStandardMaterial({
-    map: carpetTex, roughness: 0.95, metalness: 0.02
+  const base = new THREE.MeshStandardMaterial({
+    color: 0x11151f,
+    roughness: low ? 0.95 : 0.85,
+    metalness: 0.15
   });
 
-  const matConcrete = new THREE.MeshStandardMaterial({
-    color: 0x1b1f24, roughness: 0.98, metalness: 0.02
+  const wall = new THREE.MeshStandardMaterial({
+    color: 0x0b0f18,
+    roughness: 0.95,
+    metalness: 0.05
   });
 
-  const matWall = new THREE.MeshStandardMaterial({
-    color: 0x0f141a, roughness: 0.85, metalness: 0.08
+  const neonCyan = new THREE.MeshStandardMaterial({
+    color: 0x06202a,
+    emissive: 0x00e5ff,
+    emissiveIntensity: low ? 0.8 : 1.25,
+    roughness: 0.6,
+    metalness: 0.2
   });
 
-  const matTrim = new THREE.MeshStandardMaterial({
-    color: 0x1a5cff,
-    roughness: 0.25,
-    metalness: 0.65,
-    emissive: new THREE.Color(0x0c2a66),
-    emissiveIntensity: 0.65
+  const neonMagenta = new THREE.MeshStandardMaterial({
+    color: 0x1a081a,
+    emissive: 0xff2bd6,
+    emissiveIntensity: low ? 0.75 : 1.2,
+    roughness: 0.65,
+    metalness: 0.2
   });
 
-  const matGold = new THREE.MeshStandardMaterial({
-    color: 0xf3c969,
-    roughness: 0.35,
-    metalness: 0.85,
-    emissive: new THREE.Color(0x120c02),
-    emissiveIntensity: 0.25
+  const neonGreen = new THREE.MeshStandardMaterial({
+    color: 0x08150d,
+    emissive: 0x33ff66,
+    emissiveIntensity: low ? 0.7 : 1.1,
+    roughness: 0.7,
+    metalness: 0.15
   });
 
-  const matNeonPink = new THREE.MeshStandardMaterial({
-    color: 0xff2aa6,
-    roughness: 0.25,
-    metalness: 0.45,
-    emissive: new THREE.Color(0x3a001a),
-    emissiveIntensity: 0.85
+  const floor = new THREE.MeshStandardMaterial({
+    color: 0x070b10,
+    roughness: 0.95,
+    metalness: 0.05
   });
 
-  const matNeonCyan = new THREE.MeshStandardMaterial({
-    color: 0x2aa6ff,
-    roughness: 0.25,
-    metalness: 0.45,
-    emissive: new THREE.Color(0x001b33),
-    emissiveIntensity: 0.85
-  });
+  const grid = new THREE.LineBasicMaterial({ color: 0x113355 });
 
-  return {
-    carpetTex,
-    matCarpet, matConcrete, matWall,
-    matTrim, matGold, matNeonPink, matNeonCyan
-  };
+  return { base, wall, neonCyan, neonMagenta, neonGreen, floor, grid };
 }

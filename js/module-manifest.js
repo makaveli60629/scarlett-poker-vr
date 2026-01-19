6e0cfa4c2159c67619a8719a29d165a73081fc1a
makaveli60629 <ronaldchadee@gmail.com>
2026-01-19 16:30:44 -0600

    Add files via upload

---
+++ js/module-manifest.js
@@ -0,0 +1,9 @@
+// Edit this list to match your real modules.
+// For your audit, add paths like "./modules/world.js", "./modules/teleport.js", etc.
+export const MODULES = [
+  "./modules/world.js",
+  "./modules/teleport.js",
+  "./modules/diagnostics.js",
+  "./modules/jumbotron.js",
+  "./modules/poker_stub.js"
+];
